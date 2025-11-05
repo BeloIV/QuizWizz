1ce07062dca1613aded8fcefc9f2079d51145129
Štefan Beluško <beluskostefan@gmail.com>
2025-11-05 21:08:44 +0100

    config change

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api';
+export const API_BASE_URL =
+    process.env.REACT_APP_API_BASE_URL ||
+    `http://${window.location.hostname}:8000/api`;
