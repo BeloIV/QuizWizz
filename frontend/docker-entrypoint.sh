67f555bd5fc72e292dcccd85a0fe5a8d93c5846f
Štefan Beluško <beluskostefan@gmail.com>
2025-12-04 16:01:57 +0100

    docker-entrypoint.sh

---
+++ frontend/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+# Install dependencies if node_modules doesn't exist or is empty
+if [ ! -d "node_modules" ] || [ -z "$(ls -A node_modules)" ]; then
+    echo "Installing dependencies..."
+    npm install --legacy-peer-deps
+fi
+
+# Execute the CMD
+exec "$@"
+
