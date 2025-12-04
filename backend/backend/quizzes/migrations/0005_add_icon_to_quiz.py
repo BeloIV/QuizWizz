77d335c203e694b5c927249c9a844e00f4f76631
g-30 <8287493+g-30@users.noreply.github.com>
2025-12-04 09:33:08 +0100

    migrations deps change

--- backend/backend/quizzes/migrations/0005_add_icon_to_quiz.py
+++ backend/backend/quizzes/migrations/0005_add_icon_to_quiz.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("quizzes", "0004_seed_quizzes"),
+        ("quizzes", "0001_initial"),
     ]
 
     operations = [
